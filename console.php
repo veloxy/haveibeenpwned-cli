c80a30c5f64f2b19d6d8b2825f39c89354f3b997
Kevin Vandenborne <kevin.vandenborne@gmail.com>
2016-10-08 14:20:29 +0200

    Added service container

--- console.php
+++ console.php
@@ -1,8 +1,23 @@
 #!/usr/bin/env php
 <?php
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Sourcebox\HaveIBeenPwnedCLI\Console\Application;
+
 require __DIR__.'/vendor/autoload.php';
 
 ini_set("auto_detect_line_endings", '1');
 
-$application = new Sourcebox\HaveIBeenPwnedCLI\Console\Application();
+$container = new ContainerBuilder();
+
+$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/src/Resources/config/'));
+$loader->load('services.yml');
+
+$application = new Application();
+
+foreach ($container->findTaggedServiceIds('console.command') as $taggedServiceId => $attributes) {
+    $application->add($container->get($taggedServiceId));
+}
+
 $application->run();
